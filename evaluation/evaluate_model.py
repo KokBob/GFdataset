5815c2ac7353add9a60674c6a43484aee9d07775
Marek Ciklamini <marekciklamini@gmail.com>
2023-03-24 11:34:43 +0100

    Create evaluate_model.py

---
+++ evaluation/evaluate_model.py
@@ -0,0 +1,4 @@
+''' refs
+https://medium.datadriveninvestor.com/data-visualization-using-plotly-in-python-aeb44d65ea38
+[How to make multi-index index charts with Plotly] https://towardsdatascience.com/how-to-make-multi-index-index-charts-with-plotly-4d3984cd7b09
+'''
