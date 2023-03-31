c1647fff67b807cd41e98f665321768215b6033d
Marek Ciklamini <marekciklamini@gmail.com>
2023-03-31 15:57:19 +0200

    Update training_beam3D_full_GNN_03.py

--- ffnn/training_beam3D_full_GNN_03.py
+++ ffnn/training_beam3D_full_GNN_03.py
@@ -19,18 +19,34 @@ pathRes  = GFDS.pathRes
 methodID = 'GCN_RF2' 
 MODEL = GCN0
 D = GFDS.D
+
+
 X0 = GFDS.X0
 y = GFDS.y
 G = GFDS.G
 df = pd.DataFrame(X0)
 dfy = pd.DataFrame(y)
 
+# scaler = MinMaxScaler()
+# X0 = pd.DataFrame(scaler.fit_transform(df), columns=df.columns).values
+
 graphs = graphs_preparation(D, G, X0, y)
-experiments_IDs = [42]
+experiments_IDs = [42,]
 num_epochs      = 10
 # %% training loop pro gcn
 for experiment_number in experiments_IDs:
+    
     exp_ = exps.Experiment_Graph(gfds_name, methodID, experiment_number, graphs)
+    pathModel = pathRes + exp_.experiment_name + '.pt'
     exp_.training_preparation(MODEL)
-    exp_.training_run(num_epochs)
+    # exp_.training_run(num_epochs)
+    # exp_.validate()
+    # torch.save(exp_.model.state_dict(),pathModel)
+    
+    # exp_.training_preparation(MODEL)
+    # model = exp_.model
+    
+    exp_.model.load_state_dict(torch.load(pathModel))
+    exp_.model.eval()
     exp_.validate()
+    # model.load_state_dict(torch.load(pathModel))
