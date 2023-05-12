2a57919bd9ddcb8f30621b6e25540b7d7d59546c
Marek Ciklamini <marekciklamini@gmail.com>
2023-05-12 14:51:32 +0200

    Add files via upload

---
+++ ffnn/training_dev/training_plane_full_GNN_05.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+training_execute = True
+# training_execute = False
+experiment_number = 42
+import sys
+import pandas as pd
+sys.path.append("..") 
+from load_dataset import load_dataset 
+from modelling.modelsStore import GCN0
+from modelling.modelsStore import SAGE0
+from modelling.preprocessing import *
+import modelling.experimentationing as exps
+import torch
+GFDS = load_dataset.Plane()
+gfds_name   = GFDS.gfds_name
+pathRes     = GFDS.pathRes
+D           = GFDS.D
+
+X0 = GFDS.X0
+y = GFDS.y
+G = GFDS.G
+df = pd.DataFrame(X0)
+dfy = pd.DataFrame(y)
+
+scaler = MinMaxScaler()
+X0 = pd.DataFrame(scaler.fit_transform(df), columns=df.columns).values
+
+graphs = graphs_preparation(D, G, X0, y)
+experiments_IDs = [42]
+num_epochs      = 500
+# %% training loop pro gcn
+if training_execute: 
+    methodID    = 'Xminmax_GCN_RF2' 
+    MODEL       = GCN0
+    for experiment_number in experiments_IDs:
+        
+        exp_ = exps.Experiment_Graph(gfds_name, methodID, experiment_number, graphs)
+        pathModel = pathRes + exp_.experiment_name + '.pt'
+        exp_.training_preparation(MODEL)
+        # training 
+        exp_.training_run(num_epochs)
+        exp_.validate()
+        torch.save(exp_.beast,pathModel)
+# %% training loop pro sage
+    methodID    = 'Xminmax_SAGE_RF2' 
+    MODEL       = SAGE0
+    for experiment_number in experiments_IDs:
+        
+        exp_ = exps.Experiment_Graph(gfds_name, methodID, experiment_number, graphs)
+        pathModel = pathRes + exp_.experiment_name + '.pt'
+        exp_.training_preparation(MODEL)
+        # training 
+        exp_.training_run(num_epochs)
+        exp_.validate()
+        # torch.save(exp_.model.state_dict(),pathModel)
+        torch.save(exp_.beast,pathModel)
+else: pass
+# %% Loading from trained    
+# methodID    = 'GCN_RF2' 
+methodID    = 'Xminmax_SAGE_RF2' 
+MODEL       = GCN0
+exp_ = exps.Experiment_Graph(gfds_name, methodID, experiment_number, graphs)
+pathModel = pathRes + exp_.experiment_name + '.pt'
+exp_.training_preparation(MODEL)
+exp_.model.load_state_dict(torch.load(pathModel))
+exp_.model.eval()
+exp_.validate()
+exp_.validate_plot()
+    
