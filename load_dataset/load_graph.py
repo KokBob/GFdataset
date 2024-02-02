2339e31997775b19e8766232dfae694b83c24899
Marek Ciklamini <marekciklamini@gmail.com>
2024-02-02 13:23:00 +0100

    Update load_graph.py

--- load_dataset/load_graph.py
+++ load_dataset/load_graph.py
@@ -4,6 +4,7 @@
 import sys
 sys.path.append("..") #
 from gnsfem import preprocessing
+from pathlib import Path
 
 class graphset(object):
     def __init__(self, input_file_name, endDef):
@@ -12,11 +13,43 @@ class graphset(object):
         self.d_nodes         = preprocessing.get_nodes(input_file_name, line_elements,line_nodes)
         self.d_elements      = preprocessing.get_edges(input_file_name, line_elements,line_nodes, line_end)
         pass
-    
-
+class Plane(object):
+    def __init__(self, ):
+        self.input_file_name = '../datasets/pl/Plane.inp'  # Plane
+        self.path_ = Path(self.input_file_name)
+        endDef = '*Nset, nset=RC-1_Set-All-Material, generate'
+        self.graph_set = graphset(self.input_file_name, endDef = endDef)
+        self.d_nodes = self.graph_set.d_nodes
+        self.d_elements = self.graph_set.d_elements
+        self.Gcoor = preprocessing.create_graph_coor_tetra(self.d_nodes, self.d_elements) 
+        self.G0 = preprocessing.create_graph_coor_tetra(self.d_nodes, self.d_elements)
+        lG0 = [self.G0.nodes[tnode].pop('pos',None) for tnode in self.G0.nodes]
+class Fibonacci(object):
+    def __init__(self, ):
+        self.input_file_name = '../datasets/fs/Fibonacci.inp' 
+        self.path_ = Path(self.input_file_name)
+        endDef = '*Elset, elset=Set-All, generate'
+        self.graph_set = graphset(self.input_file_name, endDef = endDef)
+        self.d_nodes = self.graph_set.d_nodes
+        self.d_elements = self.graph_set.d_elements
+        self.Gcoor = preprocessing.create_graph_coor(self.d_nodes, self.d_elements)# C3D8 Hexa
+        self.G0 = preprocessing.create_graph_coor(self.d_nodes, self.d_elements)
+        lG0 = [self.G0.nodes[tnode].pop('pos',None) for tnode in self.G0.nodes]
+class Beam3D(object):
+    def __init__(self, ):
+        self.input_file_name = '../datasets/b3/Beam3D.inp' 
+        self.path_ = Path(self.input_file_name)
+        endDef = '*Elset, elset=Set-All, generate'
+        self.graph_set = graphset(self.input_file_name, endDef = endDef)
+        self.d_nodes = self.graph_set.d_nodes
+        self.d_elements = self.graph_set.d_elements
+        self.Gcoor = preprocessing.create_graph_coor(self.d_nodes, self.d_elements)# C3D8 Hexa
+        self.G0 = preprocessing.create_graph_coor(self.d_nodes, self.d_elements)
+        lG0 = [self.G0.nodes[tnode].pop('pos',None) for tnode in self.G0.nodes]
 class Beam2D(object):
     def __init__(self, ):
         self.input_file_name = '../datasets/b2/Beam2D.inp' 
+        self.path_ = Path(self.input_file_name)
         endDef = '*Nset, nset=Set-Part, generate'
         self.graph_set = graphset(self.input_file_name, endDef = endDef)
         self.d_nodes = self.graph_set.d_nodes
@@ -24,7 +57,3 @@ class Beam2D(object):
         self.Gcoor = preprocessing.create_graph_coor_2D(self.d_nodes, self.d_elements)
         self.G0 = preprocessing.create_graph_coor_2D(self.d_nodes, self.d_elements)
         lG0 = [self.G0.nodes[tnode].pop('pos',None) for tnode in self.G0.nodes]
-# Test
-# test = Beam2D()
-# G = test.Gcoor
-# G0 = test.G0
